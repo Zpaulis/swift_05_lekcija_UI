6b8afcaf6e74ec2a5e91a5ef045c43ff333a4345
Zpaulis <paulis@zabarovski.lv>
2022-04-16 16:13:02 +0300

    Initial Commit

---
+++ swift_05_lekcija_UI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift_05_lekcija_UI
+//
+//  Created by Paulis Zabarovskis on 16/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
