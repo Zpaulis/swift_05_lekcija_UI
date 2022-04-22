f1b3b6bfb170baabf9f0543ba5a90d5f101d2979
Zpaulis <paulis@zabarovski.lv>
2022-04-23 02:58:50 +0300

    6. lekcija Commit2

--- swift_05_lekcija_UI/SecondViewController.swift
+++ swift_05_lekcija_UI/SecondViewController.swift
@@ -10,10 +10,31 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
+    //MARK: - Input parametrs
+    var gameTitle: String?
+    var gameDescription: String?
+    var gameScreen: UIImage?
+    
+    
+    
+    //MARK: - Outlets
+    @IBOutlet weak var gameTitleLabel: UILabel!
+    @IBOutlet weak var gameScreenLabel: UIImageView!
+    @IBOutlet weak var gameDescriptionLabel: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-      //
+    
+        // Definējam scēnas virsrakstu
         self.title = "Otrais ekrāns"
+        // Set data
+        self.gameTitleLabel.text = self.gameTitle
+        self.gameScreenLabel.image = self.gameScreen
+        self.gameDescriptionLabel.text = self.gameDescription
+        
         
     }
     
